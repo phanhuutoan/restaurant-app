d48b39ddfb3736b211a2aa69aa54c757b2fac5b7
Mac <mac@Macs-MacBook-Pro.local>
2024-09-16 12:11:37 +0700

    Edge finish

--- src/server/prisma.ts
+++ src/server/prisma.ts
@@ -2,30 +2,20 @@
  * Instantiates a single instance PrismaClient and save it on the global object.
  * @link https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices
  */
-import { neonConfig, Pool } from '@neondatabase/serverless';
 import { env } from './env';
-import { PrismaClient } from '@prisma/client';
-import ws from 'ws';
-import { PrismaNeon } from '@prisma/adapter-neon';
-
-neonConfig.webSocketConstructor = ws;
-const connectionString = `${env.DATABASE_URL}`;
-console.log('CONNECTING TO DATABASE:', connectionString);
-
-const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool);
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
 const prismaGlobal = globalThis as typeof globalThis & {
-  prisma?: PrismaClient;
+  prisma?: any;
 };
 
-export const prisma: PrismaClient =
+export const prisma =
   prismaGlobal.prisma ??
   new PrismaClient({
-    adapter: adapter,
     log:
       env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-  });
+  }).$extends(withAccelerate());
 
 if (env.NODE_ENV !== 'production') {
   prismaGlobal.prisma = prisma;
